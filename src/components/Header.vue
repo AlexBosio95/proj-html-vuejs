ca4c1fbf6b51ed934589bce6b71bcd3165b81f85
AlexBosio95 <alex.bosio1995@gmail.com>
2022-07-26 13:17:40 +0200

    feat: header and menu items

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,43 @@
 <template>
-  
+
+    <header>
+        <div class="container">
+            <div class="row row-cols-2">
+                <div class="col logo-container">
+                    <img src="../assets/charity/images/avada-charity-logo.png" alt="">
+                </div>
+                <div class="col">
+                    <NavBar />
+                </div>
+            </div>
+        </div>
+        
+    </header>
+
 </template>
 
 <script>
+import NavBar from './NavBar.vue';
+
+
 export default {
+    components: {
+        NavBar,
+    }
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+header{
+    background-image: url('../assets/charity/images/home-page-slider.jpg');
+    background-size: cover;
+
+    .logo-container img{
+        height: 3rem;
+        margin: 1rem 0rem;
+    }
+}
 
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,76 @@
+<template>
+    <nav >
+        <ul class="d-flex align-items-center">
+            <li v-for="(item, index) in navItems" :key="index">{{item.text}}</li>
+        </ul>
+    </nav>
+  
+</template>
+
+<script>
+import Home from './Home.vue';
+import Mission from './Mission.vue';
+import Causes from './Causes.vue';
+import Journal from './Journal.vue';
+
+
+
+export default {
+    components:{
+        Home,
+        Mission,
+        Causes,
+        Journal,
+    },
+    data: function(){
+        return{
+            navItems: [
+                {
+                    text: 'Home',
+                    isClick: false,
+                    link: '',
+                },
+                {
+                    text: 'Mission',
+                    isClick: false,
+                    link: '',
+                },
+                {
+                    text: 'Causes',
+                    isClick: false,
+                    link: '',
+                },
+                {
+                    text: 'Journal',
+                    isClick: false,
+                    link: '',
+                },
+            ]
+
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    ul{
+        list-style: none;
+        height: 3rem;
+        margin: 1rem 0rem;
+
+        li{
+            color: white;
+            display: inline;
+            margin: 0rem 2rem;
+            text-transform: uppercase;
+        }
+
+        .active{
+            
+        }
+    }
+
+
+
+</style>
