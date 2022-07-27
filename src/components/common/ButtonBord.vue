30b1a97dc87d2c80bc2940769b15d045209a8225
AlexBosio95 <alex.bosio1995@gmail.com>
2022-07-27 19:44:13 +0200

    feat: hover button with border

--- src/components/common/ButtonBord.vue
+++ src/components/common/ButtonBord.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <button :class="{'donate' : (isCurrent)}">{{text}}</button>
-        
     </div>
   
 </template>
@@ -27,6 +26,12 @@ export default {
         padding: 0.3rem 1.3rem;
         text-transform: uppercase;
         margin: 0 1.5rem;
+
+        &:hover{
+            background-color: $white;
+            color: $goldenrod;
+            border: 2px solid $goldenrod;
+        }
     }
 
     .donate{
